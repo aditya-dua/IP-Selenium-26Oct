f8b2a13d74e5f268f6c1949cec77b98b0425a7e9
Aditya Dua <adityaduatechm@gmail.com>
2022-10-28 08:48:16 +0530

    First Commit
    First Commit

---
+++ CoreJavaBasics/src/DataTypesExampleMain.java
@@ -0,0 +1,26 @@
+
+public class DataTypesExampleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10; // -tive and +tive values
+		boolean admin = true; // 2 values... true or false
+		char c ='a'; // different chars
+		
+		if(a>10) {
+			System.out.println("a is greater than 10.");
+		}else {
+			System.out.println("a is less than 10.");
+		}
+		
+		if(admin) {
+			System.out.println(admin);
+			System.out.println(a);
+			System.out.println(c);
+		}else {
+			System.out.println("No Values will be Printed.");
+		}
+	}
+
+}
